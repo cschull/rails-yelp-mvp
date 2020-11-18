c4a82b2ab40d57dea6f99ef9610d3262572efba1
Camille Schull <camille.schull@gmail.com>
2020-11-18 15:16:42 +0100

    Restaurant challenge

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show ]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      raise
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    strong_params = params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants do
-    resources :reviews, only: %i[':index' ':new' ':create']
+    resources :reviews, only: %i[index new create]
   end
 
-  resources :comments, only: %i[':show' ':edit' ':update' ':destroy']
+  resources :reviews, only: %i[show edit update destroy]
 end
